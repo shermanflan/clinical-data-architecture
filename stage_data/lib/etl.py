0d1fd56a1a35bfcb0920351748083b050dfdbc63
shermanflan <shermanflan@gmail.com>
2021-10-23 13:09:58 -0500

    Add partitions to delta table via inline create

--- stage_data/lib/etl.py
+++ stage_data/lib/etl.py
@@ -5,7 +5,7 @@ from os.path import basename
 from delta.tables import DeltaTable
 from pyspark.sql.functions import (
     asc, col, concat, count, countDistinct, expr,
-    lit, sum, to_timestamp, to_date, year
+    DataFrame, lit, sum, to_timestamp, to_date, year
 )
 from pyspark.sql.types import (
     ArrayType, BooleanType, DateType, DecimalType,
@@ -20,15 +20,18 @@ from lib.schema import VITALS, CQ_VITALS
 
 def create_vitals_delta(session, delta_root: str) -> str:
     """
+    TODO: The `partitionedBy` routine is not creating the partition metadata.
+    For now, the Delta table will be created inline during initial
+    staging. Try converting to Spark SQL DDL.
 
     :return: the path to the delta table
     """
     delta_path = "{root}/public/vitals/delta".format(root=delta_root)
 
-    # TODO: Add constraints (update trigger, sequence) via generated columns
     (
         DeltaTable
         .createIfNotExists(session)
+        .tableName("delta_vitals")
         .addColumn("client_id", StringType())
         .addColumn("source_ale_prac_id", "INT",
                    comment="Source practice id")
@@ -46,18 +49,9 @@ def create_vitals_delta(session, delta_root: str) -> str:
         .addColumn("created_at", TimestampType(), nullable=False)
         .addColumn("updated_at", TimestampType(), nullable=False)
         .property("description", "published vitals")
-        .location(delta_path)
         .partitionedBy("source_ale_prac_id")
+        .location(delta_path)
         .execute()
-    )
-
-    logger.info(f"Create delta temp view: {delta_path}")
-    (
-        session
-        .read
-        .format("delta")
-        .load(delta_path)  # As DataFrame
-        .createOrReplaceTempView("delta_vitals")
     )
 
     # (
@@ -75,11 +69,11 @@ def create_vitals_delta(session, delta_root: str) -> str:
     return delta_path
 
 
-def cache_mpmi(session) -> None:
+def cache_mpmi(session) -> DataFrame:
     """
     """
     logger.info("Load mc_practice_master_info and cache to memory")
-    (
+    mpmi = (
         session
         .read
         .jdbc(url=os.environ['POSTGRES_JDBC_URL'],
@@ -88,11 +82,12 @@ def cache_mpmi(session) -> None:
         .select('document_oid', 'ale_prac_id')
         .distinct()
         .cache()
-        .createOrReplaceTempView("mpmi")
     )
+    mpmi.createOrReplaceTempView("mpmi")
+    return mpmi
 
 
-def stage_data(session, input_path: str, output_path: str) -> None:
+def stage_data(session, mpmi: DataFrame, input_path: str, output_path: str) -> None:
     """
     """
     logger.info(f"Read vitals: {input_path}")
@@ -103,9 +98,18 @@ def stage_data(session, input_path: str, output_path: str) -> None:
         # .option("nullValue", "")  # Replace any null data with quotes
         .csv(input_path,
              sep='|', header=False, schema=VITALS)
+        .drop('height_cm', 'height_comment', 'weight_kg', 'weight_comment',
+              'temperature_c', 'temperature_f', 'temperature_comment',
+              'pulse', 'pulse_comment', 'respiration', 'respiration_comment',
+              'bp_comment', 'oxygen_saturation', 'oxygen_comment',
+              'bmi_comment', 'clinician', 'comment', 'client_vitals_id')
         .withColumn("source", lit(basename(input_path)))
         .withColumn("created_at", to_timestamp(lit(ETL_DATE), "yyyy-MM-dd HH:mm:ss"))
         .withColumn("updated_at", to_timestamp(lit(ETL_DATE), "yyyy-MM-dd HH:mm:ss"))
+        .alias('s')
+        .join(mpmi.alias('m'), col("s.client_id") == col("m.document_oid"))
+        .drop('document_oid')
+        .withColumnRenamed('ale_prac_id', 'source_ale_prac_id')
     )
 
     stage_path = (
@@ -116,13 +120,13 @@ def stage_data(session, input_path: str, output_path: str) -> None:
                 D=date.today().day)
     )
 
-    logger.info(f"Stage vitals, prac partitioned: {stage_path}")
+    logger.info(f"Stage vitals, partitioned by practice: {stage_path}")
     (
         raw
         .write
         .parquet(stage_path,
                  mode='overwrite',
-                 partitionBy=['client_id'])  # action
+                 partitionBy=['source_ale_prac_id'])  # action
     )
 
     logger.info(f"Create stage temp view: {stage_path}")
@@ -134,16 +138,16 @@ def stage_data(session, input_path: str, output_path: str) -> None:
     )
 
 
-def load_vitals(session, input_path: str, output_path: str) -> None:
+def load_vitals(session, mpmi: DataFrame, input_path: str, output_path: str) -> None:
     """
+    Initial load which creates the partitioned Delta table.
     """
-    stage_data(session, input_path, output_path)
+    stage_data(session, mpmi, input_path, output_path)
 
-    # TODO: De-duplicate by comparing row hash against delta cq
-    logger.info(f"Conform vitals")
+    logger.info(f"Conform vitals (load)")
     conformed = session.sql("""
         SELECT  v.client_id,
-                m.ale_prac_id AS source_ale_prac_id,
+                v.source_ale_prac_id,
                 v.encounter_id,
                 v.patient_id,
                 STACK(5,
@@ -165,16 +169,11 @@ def load_vitals(session, input_path: str, output_path: str) -> None:
                 v.created_at,
                 v.updated_at
         FROM    stage_vitals AS v
-            INNER JOIN mpmi AS m
-                ON v.client_id = m.document_oid
-            LEFT JOIN delta_vitals AS delta
-                ON v.row_hash = delta.source_guid
-        WHERE   delta.source_guid IS NULL
     """)
 
     delta_path = "{root}/public/vitals/delta".format(root=output_path)
 
-    logger.info(f"Publish vitals delta: {delta_path}")
+    logger.info(f"Create vitals delta: {delta_path}")
     (
         conformed
         .filter(conformed.value.isNotNull())
@@ -195,35 +194,42 @@ def load_vitals(session, input_path: str, output_path: str) -> None:
                 'updated_at')
         .write
         # .partitionBy('source_ale_prac_id')
-        .format('delta')
-        .mode("append")
-        # .option("mergeSchema", "true")
-        .save(delta_path)
+        # .format('delta')
+        # .mode("append")
+        # .save(delta_path)
+        # Cleaner option
+        .partitionBy('source_ale_prac_id')
+        .option('path', delta_path)
+        .saveAsTable('delta_vitals', format='delta', mode='overwrite')
     )
 
-    logger.info(f"Read vitals delta: {delta_path}")
-    (
-        session
-        .read
-        .format("delta")
-        .load(delta_path)  # As DataFrame
-        # .groupBy('source_ale_prac_id')
-        # .count()
-        .show(n=21, truncate=False)
-    )
+    # logger.info(f"Read vitals delta: {delta_path}")
+    # (
+    #     session
+    #     .read
+    #     .format("delta")
+    #     .load(delta_path)  # As DataFrame
+    #     .groupBy('source_ale_prac_id')
+    #     .count()
+    #     .orderBy('source_ale_prac_id')
+    #     .show(n=21, truncate=False)
+    # )
 
 
-def upsert_vitals(session, input_path: str, output_path: str) -> None:
+def upsert_vitals(session, mpmi: DataFrame, input_path: str, output_path: str) -> None:
     """
     """
-    stage_data(session, input_path, output_path)
+    stage_data(session, mpmi, input_path, output_path)
 
-    logger.info(f"Conform vitals")
-    conformed = session.sql("""
+    delta_path = "{root}/public/vitals/delta".format(root=output_path)
+
+    logger.info(f"Conform vitals (upsert)")
+    conformed = session.sql(f"""
         WITH clean_vitals
         AS
         (
             SELECT  sv.client_id,
+                    sv.source_ale_prac_id,
                     sv.encounter_id,
                     sv.patient_id,
                     sv.height_in,
@@ -252,12 +258,12 @@ def upsert_vitals(session, input_path: str, output_path: str) -> None:
                                 to_timestamp(sv.service_date, 'yyyyMMdd')) DESC
                     ) AS rn
             FROM    stage_vitals AS sv
-                LEFT JOIN delta_vitals AS delta
+                LEFT JOIN delta.`{delta_path}` AS delta
                     ON sv.row_hash = delta.source_guid
             WHERE   delta.source_guid IS NULL
         )
         SELECT  v.client_id,
-                m.ale_prac_id AS source_ale_prac_id,
+                v.source_ale_prac_id,
                 v.encounter_id,
                 v.patient_id,
                 STACK(5,
@@ -276,14 +282,10 @@ def upsert_vitals(session, input_path: str, output_path: str) -> None:
                 v.created_at,
                 v.updated_at
         FROM    clean_vitals AS v
-            INNER JOIN mpmi AS m
-                ON v.client_id = m.document_oid
         WHERE   v.rn = 1
     """)
 
-    delta_path = "{root}/public/vitals/delta".format(root=output_path)
-
-    logger.info(f"Publish vitals delta: {delta_path}")
+    logger.info(f"Merge vitals delta: {delta_path}")
     (
         DeltaTable
         .forPath(session, delta_path).alias('tgt')
@@ -310,16 +312,17 @@ def upsert_vitals(session, input_path: str, output_path: str) -> None:
         .execute()
     )
 
-    logger.info(f"Read vitals delta: {delta_path}")
-    (
-        session
-        .read
-        .format("delta")
-        .load(delta_path)  # To DataFrame
-        # .groupBy('source_ale_prac_id')
-        # .count()
-        .show(n=21)
-    )
+    # logger.info(f"Read vitals delta: {delta_path}")
+    # (
+    #     session
+    #     .read
+    #     .format("delta")
+    #     .load(delta_path)  # To DataFrame
+    #     .groupBy('source_ale_prac_id')
+    #     .count()
+    #     .orderBy('source_ale_prac_id')
+    #     .show(n=21)
+    # )
 
 
 def time_travel(session, delta_path: str) -> None:
